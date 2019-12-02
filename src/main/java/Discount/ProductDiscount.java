48ca034976d9f992c944bd9f06954ad453e31206
Lucia Velez <lucia.velez@lottoland.com>
2019-12-02 17:12:27 +0100

    add log for invalid discounts to a product

--- src/main/java/Discount/ProductDiscount.java
+++ src/main/java/Discount/ProductDiscount.java
@@ -3,11 +3,17 @@ package Discount;
 import ShoppingCart.ShoppingCart;
 import lombok.Data;
 import lombok.NonNull;
+import org.apache.log4j.Logger;
 
 import java.math.BigDecimal;
 
+import static org.apache.log4j.helpers.UtilLoggingLevel.INFO;
+
 @Data
 public class ProductDiscount implements Discount {
+
+    final static Logger logger = Logger.getLogger(ProductDiscount.class);
+
     @NonNull private DiscountType type;
     @NonNull private BigDecimal quantity;
     @NonNull private String productName;
@@ -16,7 +22,26 @@ public class ProductDiscount implements Discount {
         cart.getProducts()
         .stream()
         .filter(p -> p.getName().equals(productName))
-        .forEach(productToDiscount -> productToDiscount.setPrice(getDiscountedAmount(productToDiscount.getPrice())));
+        .forEach(productToDiscount -> {
+            if(isValidDiscount(productToDiscount.getPrice())) {
+                productToDiscount.setPrice(getDiscountedAmount(productToDiscount.getPrice()));
+            } else {
+                logger.log(INFO, "Product discount not valid and not applied -> " + this.toString());
+            }
+        });
+    }
+
+    private boolean isValidDiscount(@NonNull BigDecimal price) {
+        if((this.type.equals(DiscountType.PERCENTAGE)
+                && (this.quantity.compareTo(new BigDecimal(100)) == 1))) {
+            return false;
+        }
+
+        if(this.type == DiscountType.ABSOLUTE && this.quantity.compareTo(price) == 1) {
+            return false;
+        }
+
+        return true;
     }
 
     private BigDecimal getDiscountedAmount(BigDecimal amount) {
--- src/test/java/Discount/ProductDiscountTest.java
+++ src/test/java/Discount/ProductDiscountTest.java
@@ -4,9 +4,13 @@ import Product.Product;
 import Product.ProductLine;
 import ShoppingCart.ShoppingCart;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -55,5 +59,32 @@ class ProductDiscountTest {
         //And the cart total is updated
         assertEquals(new BigDecimal(1750).setScale(1, RoundingMode.HALF_UP), cart.calculateTotals());
 
+    }
+
+    @ParameterizedTest
+    @MethodSource("createParametersForInvalidDiscountsTest")
+    void should_not_apply_invalid_discount_percentage(DiscountType type, BigDecimal discount) {
+        //Given a product in a shopping cart
+        ShoppingCart cart = new ShoppingCart(CustomerGroup.NEW);
+        Product product = new Product(MACBOOK, new BigDecimal(3500), new ProductLine(LAPTOPS));
+        cart.addProduct(product);
+
+        //Given an invalid discount for a product
+        ProductDiscount productDiscount = new ProductDiscount(type, discount, MACBOOK);
+
+        //When the discount is applied
+        productDiscount.apply(cart);
+
+        //Then the new price is the same one
+        assertEquals(new BigDecimal(3500), product.getPrice());
+
+        //And the cart total is not updated
+        assertEquals(new BigDecimal(3500).setScale(1, RoundingMode.HALF_UP), cart.calculateTotals());
+    }
+
+    private static Stream<Arguments> createParametersForInvalidDiscountsTest() {
+        return Stream.of(
+                Arguments.of(DiscountType.PERCENTAGE, new BigDecimal(120)),
+                Arguments.of(DiscountType.ABSOLUTE, new BigDecimal(4300)));
     }
 }
